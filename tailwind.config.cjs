4f8f092a34ebc6f185fbcb1b8df2d9b1ed4f7334
Gabriel Hall <adrastopoulos@gmail.com>
2023-02-12 18:22:34 -0500

    update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,25 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    fontFamily: {
+      display: ['Lato', 'sans-serif'],
+      body: ['Nunito', 'sans-serif']
+    }
   },
   daisyui: {
-    themes: ['light', 'dark']
+    themes: [
+      {
+        light: {
+          primary: '#BDCDC9',
+          'primary-content': '#F9F9F9',
+          accent: '#1F4C4C',
+          'accent-content': '#F9F9F9',
+          neutral: '#3d4451',
+          'base-100': '#F9F9F9',
+          'base-content': '#1F4C4C'
+        }
+      }
+    ]
   },
   plugins: [require('@tailwindcss/typography'), require('daisyui')]
 };
