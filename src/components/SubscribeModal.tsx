b48c606ff93a2b2fea6dbcb889c814e0355d4205
Adrastopoulos <gabriel.hall@cox.net>
2023-02-19 14:55:31 -0500

    add non-functional subscribe modal

---
+++ src/components/SubscribeModal.tsx
@@ -0,0 +1,71 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable react/function-component-definition */
+/* eslint-disable react/no-unescaped-entities */
+
+import Image from 'next/image';
+import { Categories } from 'types';
+
+export const SubscribeModal = () => (
+  <>
+    <input type="checkbox" id="subscribe-modal" className="modal-toggle" />
+    <div className="modal">
+      <div className="modal-box">
+        <div className="flex flex-col items-center gap-4">
+          <label
+            htmlFor="subscribe-modal"
+            className="btn-ghost btn-sm btn-circle btn absolute right-2 top-2"
+          >
+            ✕
+          </label>
+          <div className="relative h-36 w-36">
+            <Image src="/mail.svg" fill alt="" />
+          </div>
+          <h1 className="text-2xl font-bold">
+            Get alerts when new items are added!
+          </h1>
+          <p className="text-sm">
+            We'll send an email at the end of the day with items that have been
+            added in your selected category.
+          </p>
+          <p className="text-sm">
+            You&apos;ll get notifications for seven days following which you can
+            resubscribe for updates. You can unsubscribe at any time.
+          </p>
+          <form className="w-full font-bold">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Item Category</span>
+              </label>
+              <select className="select-bordered select select-lg w-full bg-primary">
+                {Categories.map((category) => (
+                  <option key={category} value={category}>
+                    {category}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email Address</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Type here"
+                className="input-bordered input w-full bg-primary"
+              />
+            </div>
+          </form>
+          <div className="modal-action">
+            <label
+              htmlFor="subscribe-modal"
+              className="btn-accent btn-sm btn rounded-full px-4 py-2"
+            >
+              Subscribe
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/label-has-associated-control */
+
 import ItemGrid from 'components/ItemGrid';
 import MainLayout from 'components/layout/MainLayout';
+import { SubscribeModal } from 'components/SubscribeModal';
 import { GetServerSideProps } from 'next';
 import { useState } from 'react';
 import {
@@ -14,6 +16,7 @@ import {
   FaSquare
 } from 'react-icons/fa';
 import getServerAuthSession from 'server/common/get-server-auth-session';
+import { Categories, Colors, Locations } from 'types';
 import useDrawerStore from '../stores/DrawerStore';
 
 export default function HomePage() {
@@ -21,107 +24,206 @@ export default function HomePage() {
   const [query, setQuery] = useState('');
 
   return (
-    <MainLayout>
-      <div className="flex flex-col items-center gap-4">
-        <div className="relative w-full text-white">
-          <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-            <button
-              type="submit"
-              className="focus:shadow-outline p-1 focus:outline-none"
-            >
-              <FaSearch className="pointer-events-none absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 transform" />
-            </button>
-          </span>
-          <input
-            type="text"
-            placeholder="Search lost items..."
-            className="input w-full rounded-full bg-primary pl-10 font-bold placeholder-accent font-display placeholder-opacity-70"
-            onChange={(e) => setQuery(e.target.value)}
-          />
-        </div>
-        <ul className="menu menu-horizontal justify-between w-full font-bold">
-          <li className="flex-1">
+    <>
+      <SubscribeModal />
+      <MainLayout>
+        <div className="mx-auto flex flex-col items-center gap-4">
+          <div className="relative w-full max-w-3xl text-white">
+            <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+              <button
+                type="submit"
+                className="focus:shadow-outline p-1 focus:outline-none"
+              >
+                <FaSearch className="pointer-events-none absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 transform" />
+              </button>
+            </span>
+            <input
+              type="text"
+              placeholder="Search lost items..."
+              className="input-bordered input w-full rounded-full border-4 bg-primary pl-10 font-display font-bold placeholder-accent placeholder-opacity-70"
+              onChange={(e) => setQuery(e.target.value)}
+            />
+          </div>
+          <ul className="menu menu-horizontal w-full justify-between font-bold md:hidden">
+            <li className="flex-1">
+              <button
+                type="button"
+                className="flex w-full flex-col items-center gap-1"
+              >
+                <FaSearchLocation className="h-6 w-6" />
+                <span className="text-xs leading-none">Location</span>
+              </button>
+            </li>
+            <li className="flex-1">
+              <button
+                type="button"
+                className="flex w-full flex-col items-center gap-1"
+              >
+                <FaColumns className="h-6 w-6" />
+                <span className="text-xs leading-none">Color</span>
+              </button>
+            </li>
+            <li className="flex-1">
+              <button
+                type="button"
+                className="flex w-full flex-col items-center gap-1"
+              >
+                <FaCalendar className="h-6 w-6" />
+                <span className="text-xs leading-none">Date</span>
+              </button>
+            </li>
+            <li className="flex-1">
+              <button
+                type="button"
+                className="flex w-full flex-col items-center gap-1"
+              >
+                <FaSquare className="h-6 w-6" />
+                <span className="text-xs leading-none">Category</span>
+              </button>
+            </li>
+            <li className="flex-1">
+              <label
+                htmlFor="my-drawer"
+                onClick={() => filterDrawer()}
+                className="flex w-full flex-col items-center"
+              >
+                <FaSort className="h-8 w-8" />
+              </label>
+            </li>
+          </ul>
+          <div className="divider m-0 md:hidden" />
+          <div className="flex w-full max-w-5xl gap-10">
+            <div className="hidden md:block">
+              <div className="w-80 self-start rounded-lg bg-primary p-5 text-accent">
+                <span className="text-xl font-bold">Filters</span>
+                <form className="form-control gap-4">
+                  <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                      <span className="label-text">Date Lost</span>
+                    </label>
+                    <input
+                      type="date"
+                      placeholder="Type here"
+                      className="input-bordered input input-sm w-full max-w-xs"
+                    />
+                  </div>
+                  <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                      <span className="label-text">Item Category</span>
+                    </label>
+                    <select className="select-bordered select select-sm w-full max-w-xs">
+                      {Categories.map((category) => (
+                        <option key={category} value={category}>
+                          {category}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                      <span className="label-text">Location Lost</span>
+                    </label>
+                    <select className="select-bordered select select-sm w-full max-w-xs">
+                      {Locations.map((location) => (
+                        <option key={location} value={location}>
+                          {location}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                      <span className="label-text">Color</span>
+                    </label>
+                    <select className="select-bordered select select-sm w-full max-w-xs">
+                      {Colors.map((color) => (
+                        <option key={color} value={color}>
+                          {color}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div>
+                    <button
+                      type="submit"
+                      className="btn-accent btn-sm btn rounded-full"
+                    >
+                      <span className="uppercase">Apply Filters</span>
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="flex w-full flex-col gap-4">
+              <ItemGrid query={query} />
+              <div className="prose hidden w-full rounded-lg border border-accent bg-secondary p-4 md:block">
+                <h3>Still can&apos;t find it?</h3>
+                <ul>
+                  <li>
+                    <p>
+                      Items that are deemed as having a high value or that are
+                      identifiable (e.g. student IDs) are not published on our
+                      website. Please contact the CUC Information Desk directly.
+                    </p>
+                    <button
+                      type="button"
+                      className="btn-accent btn-sm btn shadow-lg"
+                    >
+                      <span className="uppercase">More Info</span>
+                    </button>
+                  </li>
+                  <li>
+                    <p>
+                      Items can turn up at any time. Subscribe to be notified
+                      via email about items in your category that get added over
+                      the next 7 days.
+                    </p>
+                    <label
+                      htmlFor="subscribe-modal"
+                      className="btn-accent btn-sm btn shadow-lg"
+                    >
+                      <span className="uppercase">Subscribe</span>
+                    </label>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="divider m-0 md:hidden" />
+          <div className="md:hidden">
+            <div className="text-xl font-bold">Can&apos;t find it?</div>
+            <div className="mt-2 text-sm">
+              High value and identifiable items (e.g. student IDS) are not
+              published on our website. Please contact the CUC Information Desk
+              directly.
+            </div>
+          </div>
+          <div className="w-full md:hidden">
             <button
               type="button"
-              className="w-full flex flex-col items-center gap-1"
+              className="btn-outline btn-sm btn w-full shadow-lg"
             >
-              <FaSearchLocation className="w-6 h-6" />
-              <span className="text-xs leading-none">Location</span>
+              <span className="uppercase">More Info</span>
             </button>
-          </li>
-          <li className="flex-1">
-            <button
-              type="button"
-              className="w-full flex flex-col items-center gap-1"
-            >
-              <FaColumns className="w-6 h-6" />
-              <span className="text-xs leading-none">Color</span>
-            </button>
-          </li>
-          <li className="flex-1">
-            <button
-              type="button"
-              className="w-full flex flex-col items-center gap-1"
-            >
-              <FaCalendar className="w-6 h-6" />
-              <span className="text-xs leading-none">Date</span>
-            </button>
-          </li>
-          <li className="flex-1">
-            <button
-              type="button"
-              className="w-full flex flex-col items-center gap-1"
-            >
-              <FaSquare className="w-6 h-6" />
-              <span className="text-xs leading-none">Category</span>
-            </button>
-          </li>
-          <li className="flex-1">
+          </div>
+          <div className="md:hidden">
+            <div className="text-xl font-bold">Stay Updated</div>
+            <div className="mt-2 text-sm">
+              Items can show up at any time. Subscribe to be notified via email
+              about items in your category that get added in the next 7 days.
+            </div>
+          </div>
+          <div className="w-full md:hidden">
             <label
-              htmlFor="my-drawer"
-              onClick={() => filterDrawer()}
-              className="w-full flex flex-col items-center"
+              htmlFor="subscribe-modal"
+              className="btn-accent btn-sm btn w-full shadow-lg"
             >
-              <FaSort className="w-8 h-8" />
+              <span className="uppercase">Subscribe</span>
             </label>
-          </li>
-        </ul>
-        <div className="divider m-0" />
-        <ItemGrid query={query} />
-        <div className="divider m-0" />
-        <div>
-          <div className="text-xl font-bold">Can&apos;t find it?</div>
-          <div className="mt-2 text-sm">
-            High value and identifiable items (e.g. student IDS) are not
-            published on our website. Please contact the CUC Information Desk
-            directly.
           </div>
         </div>
-        <div className="w-full">
-          <button
-            type="button"
-            className="btn-outline btn-sm btn w-full shadow-lg"
-          >
-            <span className="uppercase">More Info</span>
-          </button>
-        </div>
-        <div>
-          <div className="text-xl font-bold">Stay Updated</div>
-          <div className="mt-2 text-sm">
-            Items can show up at any time. Subscribe to be notified via email
-            about items in your category that get added in the next 7 days.
-          </div>
-        </div>
-        <div className="w-full">
-          <button
-            type="button"
-            className="btn-accent btn-sm btn w-full shadow-lg"
-          >
-            <span className="uppercase">Subscribe</span>
-          </button>
-        </div>
-      </div>
-    </MainLayout>
+      </MainLayout>
+    </>
   );
 }
 
