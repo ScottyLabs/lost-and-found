a8c55fadc7db7a7853c44a9459aeecd6b57f22f9
Adrastopoulos <gabriel.hall@cox.net>
2023-03-17 21:06:52 -0400

    use built in next font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,11 @@
 import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.css';
 
-import { Lato } from '@next/font/google';
-import clsx from 'clsx';
 import type { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import { Lato } from 'next/font/google';
 import { PropsWithChildren } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { trpc } from 'utils/trpc';
@@ -21,9 +20,13 @@ const lato = Lato({
 
 function FontProvider({ children }: PropsWithChildren<{}>) {
   return (
-    <div className={clsx(lato.className)}>
+    <>
+      <style>{`
+        html {
+          font-family: ${lato.style.fontFamily}
+        }`}</style>
       <div className="antialiased">{children}</div>
-    </div>
+    </>
   );
 }
 
