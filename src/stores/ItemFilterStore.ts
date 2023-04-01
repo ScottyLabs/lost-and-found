5d010c5958ac935af35451847dbbef718372ee39
Gabriel Hall <adrastopoulos@gmail.com>
2023-04-01 17:33:04 -0400

    add filter store

---
+++ src/stores/ItemFilterStore.ts
@@ -0,0 +1,33 @@
+import { Building, Category, Color } from '@prisma/client';
+import { create } from 'zustand';
+
+type ItemFilterStore = {
+  date: Date | null;
+  category: Category | null;
+  location: Building | null;
+  color: Color | null;
+  setFilter: ({
+    date,
+    category,
+    location,
+    color
+  }: {
+    date: Date | null;
+    category: Category | null;
+    location: Building | null;
+    color: Color | null;
+  }) => void;
+};
+
+export default create<ItemFilterStore>((set) => ({
+  date: null,
+  category: null,
+  location: null,
+  color: null,
+  setFilter: ({
+    date = null,
+    category = null,
+    location = null,
+    color = null
+  }) => set(() => ({ date, category, location, color }))
+}));
