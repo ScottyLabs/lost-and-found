4a251d737f970ba20e5ebce7baaf85d2d667c979
Gabriel Hall <adrastopoulos@gmail.com>
2023-05-14 23:03:02 -0700

    create manage drawer

--- src/stores/DrawerStore.ts
+++ src/stores/DrawerStore.ts
@@ -1,15 +1,17 @@
 import { create } from 'zustand';
 
 type DrawerStore = {
-  drawer: 'navigate' | 'filter' | null;
+  drawer: 'navigate' | 'filter' | 'manage' | null;
   navigateDrawer: () => void;
   filterDrawer: () => void;
   clearDrawer: () => void;
+  manageDrawer: () => void;
 };
 
 export default create<DrawerStore>((set) => ({
   drawer: null,
   navigateDrawer: () => set(() => ({ drawer: 'navigate' })),
   filterDrawer: () => set(() => ({ drawer: 'filter' })),
-  clearDrawer: () => set(() => ({ drawer: null }))
+  clearDrawer: () => set(() => ({ drawer: null })),
+  manageDrawer: () => set(() => ({ drawer: 'manage' }))
 }));
