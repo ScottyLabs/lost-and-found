bd43a62c6ba64656ea2d8c5b65eb54ad9ffd5c78
Gabriel Hall <gabriel.hall@cox.net>
2023-07-06 22:43:16 -0700

    refactor user menu

--- src/pages/manage/users/index.tsx
+++ src/pages/manage/users/index.tsx
@@ -3,7 +3,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
 import { Popover, Transition } from '@headlessui/react';
-import { Permission, Status } from '@prisma/client';
+import { Permission } from '@prisma/client';
 import MyListbox from 'components/Form/Listbox';
 import ManageLayout from 'components/Layouts/ManageLayout';
 import UserRow from 'components/UserRow';
@@ -11,16 +11,7 @@ import useZodForm from 'hooks/useZodForm';
 import { UserSearchSchema } from 'lib/schemas';
 import { NextPageWithLayout } from 'pages/_app';
 import { Fragment } from 'react';
-import {
-  FaArchive,
-  FaCheck,
-  FaCircleNotch,
-  FaSortAmountDown,
-  FaTrash
-} from 'react-icons/fa';
-import { toast } from 'react-toastify';
-import useDialogStore from 'stores/DialogStore';
-import useSelectedUsersStore from 'stores/SelectedUserStore';
+import { FaCircleNotch, FaSortAmountDown } from 'react-icons/fa';
 import { trpc } from 'utils/trpc';
 import { z } from 'zod';
 
@@ -43,24 +34,14 @@ const UserList: React.FC<{
 };
 
 const Manage: NextPageWithLayout = () => {
-  const context = trpc.useContext();
-  const { selectedUsers, setSelectedUsers } = useSelectedUsersStore();
   const methods = useZodForm({
     schema: UserSearchSchema,
     defaultValues: {
       query: '',
-      permissions: null,
+      permissions: [],
       notifications: null
     }
   });
-  const users = trpc.user.search.useQuery(methods.getValues());
-  const itemMassUpdateMutation = trpc.item.massUpdate.useMutation({
-    onSuccess: (res) => {
-      context.item.search.invalidate();
-      toast.success(`Updated ${res.count} Items`);
-    }
-  });
-  const { confirmDeletionDialog, massArchiveDialog } = useDialogStore();
 
   return (
     <>
@@ -109,15 +90,10 @@ const Manage: NextPageWithLayout = () => {
                   <div className="flex w-full items-center justify-between p-4">
                     <div className="font-bold">Notifications</div>
                     <div className="w-48">
-                      <MyListbox
-                        control={methods.control}
-                        name="notifications"
-                        placeholder="Notification Status"
-                        displayValue={(status) =>
-                          status ? 'Enabled' : 'Disabled'
-                        }
-                        keyValue={(status) => (status ? 'y' : 'n')}
-                        values={[true, false]}
+                      <input
+                        type="checkbox"
+                        className="checkbox float-right"
+                        {...methods.register('notifications')}
                       />
                     </div>
                   </div>
@@ -135,66 +111,6 @@ const Manage: NextPageWithLayout = () => {
             </Popover>
           </div>
         </form>
-        <div className="mt-2 flex gap-2">
-          <div className="ml-3 flex flex-1 items-center">
-            <input
-              type="checkbox"
-              className="checkbox"
-              checked={
-                selectedUsers.length > 0 &&
-                selectedUsers.length === users.data?.length
-              }
-              onChange={(e) => {
-                if (!e.target.checked) setSelectedUsers([]);
-                else setSelectedUsers(users.data?.map((item) => item.id) ?? []);
-              }}
-            />
-          </div>
-          <div className="tooltip" data-tip="Approve Items">
-            <button
-              type="button"
-              className="btn-ghost btn-sm btn"
-              disabled={selectedUsers.length === 0}
-              onClick={async () =>
-                itemMassUpdateMutation.mutate({
-                  ids: selectedUsers,
-                  data: {
-                    status: Status.APPROVED
-                  }
-                })
-              }
-            >
-              <FaCheck />
-            </button>
-          </div>
-          <div className="tooltip" data-tip="Archive Items">
-            <button
-              type="button"
-              className="btn-ghost btn-sm btn"
-              disabled={selectedUsers.length === 0}
-              onClick={async () =>
-                itemMassUpdateMutation.mutate({
-                  ids: selectedUsers,
-                  data: {
-                    status: Status.ARCHIVED
-                  }
-                })
-              }
-            >
-              <FaArchive />
-            </button>
-          </div>
-          <div className="tooltip" data-tip="Delete Users">
-            <button
-              type="button"
-              className="btn-ghost btn-sm btn"
-              disabled={selectedUsers.length === 0}
-              onClick={confirmDeletionDialog}
-            >
-              <FaTrash />
-            </button>
-          </div>
-        </div>
       </div>
       <UserList
         search={{
