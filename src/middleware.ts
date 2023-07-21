399162845bcade8b9cdb69c33624635abdb1bceb
Gabriel Hall <gabriel.hall@cox.net>
2023-07-21 12:23:16 -0700

    block manage behind permission

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,23 @@
 import { Permission } from '@prisma/client';
-import { withAuth } from 'next-auth/middleware';
-
-export default withAuth({
-  callbacks: {
-    authorized({ req, token }) {
-      if (['/manage'].includes(req.nextUrl.pathname)) {
-        return !!token?.user && token.user.permission === Permission.ADMIN;
-      }
-
-      return !!token;
+import { getToken } from 'next-auth/jwt';
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+export async function middleware(request: NextRequest, _next: NextFetchEvent) {
+  const { pathname } = request.nextUrl;
+  const protectedPaths = ['/manage'];
+  const matchesProtectedPath = protectedPaths.some((path) =>
+    pathname.startsWith(path)
+  );
+  if (matchesProtectedPath) {
+    const token = await getToken({ req: request });
+    if (!token) {
+      const url = new URL(`/auth/signin`, request.url);
+      url.searchParams.set('callbackUrl', encodeURI(request.url));
+      return NextResponse.redirect(url);
+    }
+    if (token.user.permission === Permission.USER) {
+      const url = new URL(`/auth/error?error=AccessDenied`, request.url);
+      return NextResponse.rewrite(url);
     }
   }
-});
-
-export const config = {
-  matcher: ['/', '/manage/:path*']
-};
+  return NextResponse.next();
+}
