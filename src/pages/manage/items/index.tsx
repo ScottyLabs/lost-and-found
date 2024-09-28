9aefb2fb34d047ecacfc3a3f1ec9e0a6d6a0ed82
jlcvolleyball <68551447+jlcvolleyball@users.noreply.github.com>
2024-09-28 16:38:51 -0400

    changed the mass archive icon to a box so that it's different than the regular archive icon

--- src/pages/manage/items/index.tsx
+++ src/pages/manage/items/index.tsx
@@ -14,6 +14,7 @@ import { NextPageWithLayout } from 'pages/_app';
 import { Fragment } from 'react';
 import {
   FaArchive,
+  FaBox,
   FaCheck,
   FaCircleNotch,
   FaDownload,
@@ -245,7 +246,7 @@ const Manage: NextPageWithLayout = () => {
               className="btn-ghost btn-sm btn"
               onClick={massArchiveDialog}
             >
-              <FaArchive />
+              <FaBox />
             </button>
           </div>
         </div>
