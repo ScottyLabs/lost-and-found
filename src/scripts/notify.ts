a16d40ad646866b919f2f83c03965469a91a754d
axgu <annaxigu@gmail.com>
2025-03-22 16:17:22 -0400

    fix: remove text component file

--- src/scripts/notify.ts
+++ src/scripts/notify.ts
@@ -1,4 +1,3 @@
-import { mg } from '~/lib/mailgun';
 import prisma from '~/server/db/client';
 
 import { clerkClient } from '@clerk/nextjs';
@@ -19,13 +18,5 @@ async function notify() {
 
     const clerkUser = await clerkClient.users.getUser(user.externalId);
     const emailAddress = clerkUser.emailAddresses[0]!.emailAddress;
-
-    if (age > 1000 * 60 * 60 * 24 * 7) {
-    } else {
-      mg.messages.create(process.env.MAILGUN_DOMAIN, {
-        from: 'ScottyLabs Lost and Found <lostandfound@scottylabs.org>',
-        to: emailAddress
-      });
-    }
   });
 }
