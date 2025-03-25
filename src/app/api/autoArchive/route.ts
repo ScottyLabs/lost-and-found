ee9aa51985e48de444f7d8f754c46ffb3c74b60f
Xavier Lien <xavilien@gmail.com>
2025-03-25 15:18:19 -0400

    fix: middleware auth for email cron job

--- src/app/api/autoArchive/route.ts
+++ src/app/api/autoArchive/route.ts
@@ -1,5 +1,5 @@
 import { Status } from '@prisma/client';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from 'server/db/client';
 import { archived_items } from '~/emails/mailgun';
 
@@ -11,15 +11,13 @@ process.on('SIGINT', () => {
   });
 });
 
-export async function GET() {
-  // const authHeader = request.headers.get('authorization');
-  // console.log(authHeader);
-  // console.log(`Bearer ${process.env.CRON_SECRET}`);
-  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-  //   return new Response('Unauthorized', {
-  //     status: 401
-  //   });
-  // }
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401
+    });
+  }
 
   const archivedItems = await prisma.item.findMany({
     where: {
