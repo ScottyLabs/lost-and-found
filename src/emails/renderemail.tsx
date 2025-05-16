496470ff0887c4aa507078340dd014c9c4b98281
axgu <annaxigu@gmail.com>
2025-05-16 13:02:48 -0400

    send subscription email template

--- src/emails/renderemail.tsx
+++ src/emails/renderemail.tsx
@@ -1,7 +1,18 @@
 import { Item } from '@prisma/client';
 import { render } from '@react-email/render';
-import { ArchiveEmail } from '../emails/archive';
+import { ArchiveEmail } from './archive';
+import { Email, Props } from './subscription';
 
 export function renderArchiveEmail(items: Item[]) {
   return render(<ArchiveEmail items={items} />);
+}
+
+export function renderSubscriptionEmail({
+  previewText = '',
+  category,
+  items
+}: Props) {
+  return render(
+    <Email previewText={previewText} category={category} items={items} />
+  );
 }
