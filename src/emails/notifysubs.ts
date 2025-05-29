0932ad76d598a006bd7d31c6bb7d343f49f4cc3b
axgu <annaxigu@gmail.com>
2025-05-28 20:57:22 -0400

    feat: implement subscription end email

--- src/emails/notifysubs.ts
+++ src/emails/notifysubs.ts
@@ -3,14 +3,64 @@ import { Category, Status } from '@prisma/client';
 import { Categories } from 'types';
 import { send_email } from '~/emails/mailgun';
 import prisma from '~/server/db/client';
-import { renderSubscriptionEmail } from './renderemail';
+import { renderSubEndEmail, renderSubscriptionEmail } from './renderemail';
 
 const WEEK_IN_MS = 7 * 24 * 60 * 60 * 1000;
+
+type Subscription = Awaited<
+  ReturnType<typeof prisma.subscription.findMany>
+>[number];
+
+async function getEmails(subscriptions: Subscription[]) {
+  if (subscriptions.length === 0) return [];
+  const users = await Promise.all(
+    subscriptions.map((sub) =>
+      prisma.user.findUniqueOrThrow({
+        where: { id: sub.userId }
+      })
+    )
+  );
+
+  const clerkUsers = await Promise.all(
+    users.map((user) => clerkClient.users.getUser(user.externalId))
+  );
+
+  const emails = clerkUsers.map(
+    (clerkUser) => clerkUser.emailAddresses[0]!.emailAddress
+  );
+
+  return emails;
+}
 
 export async function removeExpiredSubscriptions() {
   const now = new Date();
   const validSince = new Date(now.getTime() - WEEK_IN_MS);
   try {
+    for (const category of Object.values(Category)) {
+      const cat_string = Categories[category];
+      const subscriptions = await prisma.subscription.findMany({
+        where: {
+          category,
+          createdAt: {
+            lt: validSince
+          }
+        }
+      });
+      const emails = await getEmails(subscriptions);
+      if (emails.length === 0) {
+        console.log(`No expired subscriptions for category: ${cat_string}`);
+        continue;
+      }
+
+      const subject = `Lost and Found Subscription End: ${cat_string}`;
+      const email_body = await renderSubEndEmail({
+        previewText: '',
+        category: category
+      });
+
+      await send_email(emails, subject, 'HELLO', email_body);
+    }
+
     const deleteResult = await prisma.subscription.deleteMany({
       where: {
         createdAt: {
@@ -39,21 +89,7 @@ export async function sendDailyUpdateEmails() {
       }
     });
 
-    const users = await Promise.all(
-      subscriptions.map((sub) =>
-        prisma.user.findUniqueOrThrow({
-          where: { id: sub.userId }
-        })
-      )
-    );
-
-    const clerkUsers = await Promise.all(
-      users.map((user) => clerkClient.users.getUser(user.externalId))
-    );
-
-    const emails = clerkUsers.map(
-      (clerkUser) => clerkUser.emailAddresses[0]!.emailAddress
-    );
+    const emails = await getEmails(subscriptions);
     if (emails.length === 0) {
       console.log(`No valid subscriptions for category: ${cat_string}`);
       continue;
